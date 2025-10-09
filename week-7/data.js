887c840fea1f204d76544dfb49a0f68300e5f492
Mokshita30551 <2420030551@klh.edu.in>
2025-10-09 10:20:55 +0530

    Add files via upload

---
+++ week-7/data.js
@@ -0,0 +1,7 @@
+export const books = [
+{ title: "JavaScript Essentials", author: "John Doe", price: 499, availability: "in stock"
+},
+{ title: "Harry Potter", author: "J.K. Rowling", price: 699, availability: "in stock" },
+{ title: "Node.js in Action", author: "Mark Lee", price: 599, availability: "out ofstock" },
+{ title: "Python for Everyone", author: "Ramya Krishna", price: 450, availability: "instock" }
+];
